fe6b2609f8ba137ca993e38bb3be0483d5f0d6b4
gok24code <noxonninen@gmail.com>
2026-05-10 17:18:43 +0300

    positinal fix

--- src/components/Envelope.tsx
+++ src/components/Envelope.tsx
@@ -49,10 +49,10 @@ Anneler Günün Kutlu Olsun. Seni Çok Seviyorum.`;
         {/* Letter */}
         <motion.div
           className="absolute left-4 right-4 bg-white p-6 sm:p-10 rounded shadow-lg z-20"
-          initial={{ bottom: "10%", scale: 0.95 }}
+          initial={{ bottom: "0%", scale: 0.95 }}
           animate={{
-            bottom: isOpen ? "10%" : "0%",
-            y: isOpen ? -70 : 0,
+            bottom: isOpen ? "10%" : "-10%",
+            y: isOpen ? 0 : 0,
             scale: isOpen ? 1.1 : 0.95,
             zIndex: isOpen ? 40 : 20,
           }}
