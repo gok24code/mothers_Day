efc69e6d37565e5dfe7724a6ed7de15a949e22da
gok24code <noxonninen@gmail.com>
2026-05-10 17:20:28 +0300

    positional test

--- src/components/Envelope.tsx
+++ src/components/Envelope.tsx
@@ -54,7 +54,7 @@ Anneler Günün Kutlu Olsun. Seni Çok Seviyorum.`;
             bottom: isOpen ? "10%" : "-10%",
             y: isOpen ? 40 : 0,
             scale: isOpen ? 1.1 : 0.95,
-            zIndex: isOpen ? 40 : 20,
+            zIndex: isOpen ? 40 : 20 /*commit id*/,
           }}
           transition={{ duration: 0.8, delay: isOpen ? 0.3 : 0 }}
         >
